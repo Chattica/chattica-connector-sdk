20e7ef8d22986fb9ccc35403bace272f69689d5a
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-01 01:27:25 +0900

    [ADD] Id 에 id 를 불러오는 함수 명시

--- src/main/java/com/chattica/connector/sdk/global/data/dto/id/JavaUniqueId.java
+++ src/main/java/com/chattica/connector/sdk/global/data/dto/id/JavaUniqueId.java
@@ -1,14 +1,17 @@
 package com.chattica.connector.sdk.global.data.dto.id;
 
+import com.chattica.connector.sdk.global.data._interface.Id;
+
 import java.util.UUID;
 
-public class JavaUniqueId {
+public class JavaUniqueId implements Id<String> {
     private final String id;
 
     public JavaUniqueId() {
         this.id = UUID.randomUUID().toString();
     }
 
+    @Override
     public String id() {
         return id;
     }
--- src/main/java/com/chattica/connector/sdk/global/data/dto/id/StringId.java
+++ src/main/java/com/chattica/connector/sdk/global/data/dto/id/StringId.java
@@ -2,5 +2,5 @@ package com.chattica.connector.sdk.global.data.dto.id;
 
 import com.chattica.connector.sdk.global.data._interface.Id;
 
-public record StringId(String id) implements Id {
+public record StringId(String id) implements Id<String> {
 }
