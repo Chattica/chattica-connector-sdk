3b37281b5ec98b335f276490e3fa644586f1a678
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-31 21:57:31 +0900

    [ADD] global data 관련 추상화 로직 작성

---
+++ src/main/java/com/chattica/connector/sdk/global/data/_interface/AbstractData.java
@@ -0,0 +1,4 @@
+package com.chattica.connector.sdk.global.data._interface;
+
+public interface AbstractData extends Identifiable, DataType {
+}
---
+++ src/main/java/com/chattica/connector/sdk/global/data/_interface/DataType.java
@@ -0,0 +1,5 @@
+package com.chattica.connector.sdk.global.data._interface;
+
+public interface DataType {
+    Integer getType();
+}
