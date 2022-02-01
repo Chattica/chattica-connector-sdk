16927837069dd54f44479de96a505fa98e58669c
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-01 14:00:46 +0900

    [FIX] ChatticaEventCallerFacade 에 addListener 에 대한 파사드 메서드 추가

--- src/main/java/com/chattica/connector/sdk/domain/event/caller/facade/ChatticaEventCallerFacade.java
+++ src/main/java/com/chattica/connector/sdk/domain/event/caller/facade/ChatticaEventCallerFacade.java
@@ -1,5 +1,6 @@
 package com.chattica.connector.sdk.domain.event.caller.facade;
 
+import com.chattica.connector.sdk.domain.event.caller.EventCaller;
 import com.chattica.connector.sdk.domain.event.caller.StandardEventCaller;
 import com.chattica.connector.sdk.domain.event.data.dto.Event;
 import com.chattica.connector.sdk.domain.event.data.dto.event.channel.ChannelAddEvent;
@@ -9,6 +10,10 @@ import com.chattica.connector.sdk.domain.event.data.dto.event.message.MessageAdd
 import com.chattica.connector.sdk.domain.event.data.dto.event.message.MessageRemoveEvent;
 import com.chattica.connector.sdk.domain.event.data.dto.event.message.MessageUpdateEvent;
 import com.chattica.connector.sdk.domain.event.exception.EventRoutingPathNotFound;
+import com.chattica.connector.sdk.domain.event.listener.EventListener;
+import com.chattica.connector.sdk.domain.event.listener.container.ListenerContainer;
+
+import java.util.function.BiConsumer;
 
 public class ChatticaEventCallerFacade {
     private final ChannelAddEventCaller channelAddEventCaller;
@@ -31,17 +36,31 @@ public class ChatticaEventCallerFacade {
 
     //TODO 나중에 HashMap 이랑 getType 을 통해 추상화 가능하지 않을까?
     public void routeEvent(Event event) {
-        switch (event) {
-            case ChannelAddEvent e -> channelAddEventCaller.callEvent(e);
-            case ChannelRemoveEvent e -> channelRemoveEventCaller.callEvent(e);
-            case ChannelUpdateEvent e -> channelUpdateEventCaller.callEvent(e);
+        doWithCallerByEvent(event, EventCaller::callEvent);
+    }
 
-            case MessageAddEvent e -> messageAddEventCaller.callEvent(e);
-            case MessageRemoveEvent e -> messageRemoveEventCaller.callEvent(e);
-            case MessageUpdateEvent e -> messageUpdateEventCaller.callEvent(e);
+    //TODO 뭔가 RxJava 나 Reactor 같은거로 만들 수 있을 것 같은데
+    public <T extends Event> void addListener(T event, EventListener<T> listener) {
+        doWithCallerByEvent(event, (caller, e) -> {
+            //TODO ListenerContainer 의 Generic T 와 해당 메서드의 Generic T 가 서로 일치하지 않을 경우 오류발생 즉, doWithCallerByEvent 의 반환값에 따라 오류가 발생할 수 있다.
+            if (caller instanceof ListenerContainer container) container.addListener(listener);
+            else throw new RuntimeException("해당 이벤트에 대한 caller 는 listener 추가를 지원하지 않습니다!");
+        });
+    }
+
+    private void  doWithCallerByEvent(Event event, BiConsumer<EventCaller, Event> callback) {
+        switch (event) {
+            case ChannelAddEvent e -> callback.accept(channelAddEventCaller, e);
+            case ChannelRemoveEvent e -> callback.accept(channelRemoveEventCaller, e);
+            case ChannelUpdateEvent e -> callback.accept(channelUpdateEventCaller, e);
+
+            case MessageAddEvent e -> callback.accept(messageAddEventCaller, e);
+            case MessageRemoveEvent e -> callback.accept(messageRemoveEventCaller, e);
+            case MessageUpdateEvent e -> callback.accept(messageUpdateEventCaller, e);
             default -> throw new EventRoutingPathNotFound(event.getClass().getSimpleName());
         }
     }
+
     private static class ChannelAddEventCaller extends StandardEventCaller<ChannelAddEvent> {}
     private static class ChannelRemoveEventCaller extends StandardEventCaller<ChannelRemoveEvent> {}
     private static class ChannelUpdateEventCaller extends StandardEventCaller<ChannelUpdateEvent> {}
