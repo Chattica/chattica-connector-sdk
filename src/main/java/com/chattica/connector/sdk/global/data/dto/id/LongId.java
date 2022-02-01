d8b4bf87b4f4350bac7bdb569d35258927940baa
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-02-02 01:59:15 +0900

    [REMOVE] Deprecate 된 Id 구현체 삭제

--- src/main/java/com/chattica/connector/sdk/global/data/dto/id/LongId.java
+++
@@ -1,7 +0,0 @@
-package com.chattica.connector.sdk.global.data.dto.id;
-
-import com.chattica.connector.sdk.global.data._interface.Id;
-
-@Deprecated
-public record LongId(Long id) implements Id<Long> {
-}
--- src/main/java/com/chattica/connector/sdk/global/data/dto/id/StringId.java
+++
@@ -1,7 +0,0 @@
-package com.chattica.connector.sdk.global.data.dto.id;
-
-import com.chattica.connector.sdk.global.data._interface.Id;
-
-@Deprecated
-public record StringId(String id) implements Id<String> {
-}
